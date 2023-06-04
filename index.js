b35bf76452e41e52c4758a8353f2fa0cbfb53d6a
kiyosaki-cyber <134641062+kiyosaki-cyber@users.noreply.github.com>
2023-06-04 04:26:55 +0300

    created the render function
    now the user can input their details on the input area that i can be added on to the list items function the displayed on the screen on saving

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-let myLeads = []
+let myLeads = [ "www.fantastic.com", "www.my house .com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("button-el")
-buttonEl.addEventListener("click",function buttonEl() {
+const ulEl = document.getElementById("ul-el")
+inputBtn.addEventListener("click",function buttonEl() {
     "button clicked"
     myLeads.push(inputEl.value)
-
+    renderLeads()
 })
+function  renderLeads(){
+   let listItems = " "
+    for (i = 0; i < myLeads.length; i+= 1){
+        listItems+= "<li>" + myLeads[i] +"</li>"
+            
+        }
+ulEl.innerHTML =  listItems
+}
 
